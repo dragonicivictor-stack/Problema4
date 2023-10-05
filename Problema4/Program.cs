0d82dcfa50c9213dcc9d76d30bff9c3c6eb1751f
Victor Dragonici <victor.dragonici@digitalcodify.com>
2023-10-05 10:02:21 +0300

    rezolvare problema 4 palindrom

---
+++ Problema4/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Problema4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Va rog sa introduceti cuvantul: ");
+            string text = Console.ReadLine();
+
+            int index = 0;
+            int index2 = text.Length - 1;
+            bool estepalindrom = true;
+
+
+
+            while (index < index2)
+            {
+                if (text[index] != text[index2])
+                { estepalindrom = false;
+                    break;
+                }
+                    
+                index++;
+                index2--;
+            }
+          
+
+        if(estepalindrom)
+            {
+                Console.WriteLine(text + " este palindrom.");
+            }
+        else
+            {
+                Console.WriteLine(text + " nu este palindrom.");
+            }
+        Console.ReadLine();
+        }
+    }
+}
